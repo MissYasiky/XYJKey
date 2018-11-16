33caf705222474bdc3b00c4119823cb0f69c896f
YJXie <gzxieyunjia@gmail.com>
2018-11-16 16:10:59 +0800

    数据录入页新增空白列表

--- key/XYJAddBankCardViewController.m
+++ key/XYJAddBankCardViewController.m
@@ -8,7 +8,11 @@
 
 #import "XYJAddBankCardViewController.h"
 
-@interface XYJAddBankCardViewController ()
+@interface XYJAddBankCardViewController ()<
+UITableViewDelegate,
+UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableView;
 
 @end
 
@@ -24,7 +28,23 @@
     [saveItem setTintColor:XYJColor(0x4c4c4c, 1.0)];
     self.navigationItem.leftBarButtonItem = cancelItem;
     self.navigationItem.rightBarButtonItem = saveItem;
+    
+    [self.view addSubview:self.tableView];
 }
+
+#pragma mark - Getter & Setter
+
+- (UITableView *)tableView {
+    if (_tableView == nil) {
+        _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, XYJScreenWidth(), XYJScreenHeight()) style:UITableViewStyleGrouped];
+        _tableView.backgroundColor = XYJColor(0xf4f4f4, 1.0);
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+    }
+    return _tableView;
+}
+
+#pragma mark - Action
 
 - (void)dismiss {
     [self.navigationController dismissViewControllerAnimated:YES completion:nil];
@@ -32,6 +52,71 @@
 
 - (void)save {
     [self.navigationController dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - UITableView DataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if (section == 0) {
+        return 6;
+    } else {
+        return 1;
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellIdentifier = @"cellIdentifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                                       reuseIdentifier:cellIdentifier];
+    }
+    cell.textLabel.text = @"ee";
+    return cell;
+}
+
+#pragma mark - UITableView Delegate
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.section == 0) {
+        return 42.0;
+    } else {
+        return 100.0;
+    }
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    if (section == 0) {
+        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, XYJScreenWidth(), 10)];        return view;
+    } else {
+        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, XYJScreenWidth(), 22)];
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(15, 0, 100, 22)];
+        label.font = [UIFont systemFontOfSize:14];
+        label.textColor = XYJColor(0x696969, 1.0);
+        label.text = @"备注";
+        [view addSubview:label];
+        return view;
+    }
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section {
+    return nil;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    if (section == 0) {
+        return 10.0;
+    } else {
+        return 22.0;
+    }
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    return 0.01;
 }
 
 @end
