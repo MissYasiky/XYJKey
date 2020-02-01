db127f9ae1aab00d6776f1847d0917e3250d53b7
MissYasiky <gzxieyunjia@gmail.com>
2020-02-01 22:01:56 +0800

    增加首页两个子视图控制器

--- key/Controller/XYJHomeViewController.m
+++ key/Controller/XYJHomeViewController.m
@@ -11,10 +11,21 @@
 #import "XYJHomeTabBar.h"
 
 @interface XYJHomeViewController () <
+UIPageViewControllerDelegate,
+UIPageViewControllerDataSource,
 XYJHomeTabBarDelegate
 >
 
 @property (nonatomic, strong) XYJHomeTabBar *tabBar;
+
+/// 翻页控制器
+@property (nonatomic, strong) UIPageViewController *pageViewController;
+
+/// 子视图控制器数组
+@property (nonatomic, strong) NSMutableArray <UIViewController *> *vctrlArray;
+
+/// 选中 tab 索引
+@property (nonatomic, assign) NSInteger selectedIndex;
 
 @end
 
@@ -25,7 +36,9 @@ XYJHomeTabBarDelegate
     self.view.backgroundColor = [UIColor whiteColor];
     
     [self initNavigationBar];
-    [self.view addSubview:self.tabBar];
+    [self initTabBar];
+    [self initViewControllers];
+    [self initPageViewControllers];
 }
 
 #pragma mark - Initialization
@@ -53,15 +66,49 @@ XYJHomeTabBarDelegate
     self.navigationController.interactivePopGestureRecognizer.enabled = YES;
 }
 
-#pragma mark - Getter & Setter
+- (void)initViewControllers {
+    self.vctrlArray = [[NSMutableArray alloc] initWithCapacity:2];
+    
+    UIViewController *vctrl1 = [[UIViewController alloc] init];
+    vctrl1.view.backgroundColor = [UIColor purpleColor];
+    [self.vctrlArray addObject:vctrl1];
+    
+    UIViewController *vctrl2 = [[UIViewController alloc] init];
+    vctrl2.view.backgroundColor = [UIColor brownColor];
+    [self.vctrlArray addObject:vctrl2];
+}
 
-- (XYJHomeTabBar *)tabBar {
-    if (_tabBar == nil) {
-        CGFloat originY = self.navigationController.navigationBar.frame.origin.y +  self.navigationController.navigationBar.frame.size.height;
-        _tabBar = [[XYJHomeTabBar alloc] initWithFrame:CGRectMake(0, originY, XYJScreenWidth(), 35)];
-        _tabBar.delegate = self;
+- (void)initPageViewControllers {
+    self.pageViewController = [[UIPageViewController alloc] initWithTransitionStyle:UIPageViewControllerTransitionStyleScroll
+                                                    navigationOrientation:UIPageViewControllerNavigationOrientationHorizontal
+                                                                  options:nil];
+    self.pageViewController.delegate = self;
+    self.pageViewController.dataSource = self;
+    
+    float originX = 0;
+    float originY = self.tabBar.frame.origin.y + self.tabBar.frame.size.height;
+    float width = XYJScreenWidth();
+    float height = XYJScreenHeight() - originY;
+    self.pageViewController.view.frame = CGRectMake(originX, originY, width, height);
+    
+    NSArray *initControllers = @[self.vctrlArray[0]];
+    [self.pageViewController setViewControllers:initControllers direction:UIPageViewControllerNavigationDirectionForward animated:NO completion:nil];
+    
+    [self addChildViewController:self.pageViewController];
+    [self.pageViewController didMoveToParentViewController:self];
+    [self.view addSubview:self.pageViewController.view];
+    
+    self.pageViewController.view.clipsToBounds = NO;
+    for (UIView *subview in self.pageViewController.view.subviews) {
+        subview.clipsToBounds = NO;
     }
-    return _tabBar;
+}
+
+- (void)initTabBar {
+    CGFloat originY = self.navigationController.navigationBar.frame.origin.y +  self.navigationController.navigationBar.frame.size.height;
+    self.tabBar = [[XYJHomeTabBar alloc] initWithFrame:CGRectMake(0, originY, XYJScreenWidth(), 35)];
+    self.tabBar.delegate = self;
+    [self.view addSubview:self.tabBar];
 }
 
 #pragma mark - Action
@@ -71,10 +118,68 @@ XYJHomeTabBarDelegate
     [self.navigationController pushViewController:vctrl animated:YES];
 }
 
+#pragma mark - Private
+
+#pragma mark Private - ViewCtrollers Related
+
+-(UIViewController *)viewControllerAtIndex:(NSUInteger)index {
+    if (index < 0 || index >= self.vctrlArray.count ) {
+        return nil;
+    }
+    return self.vctrlArray[index];
+}
+
+-(NSUInteger)indexOfViewController:(UIViewController *)viewController {
+    return [self.vctrlArray indexOfObject:viewController];
+}
+
 #pragma mark - XYJHomeTabBar Delegate
 
 - (void)selectTabBarAtIndex:(NSInteger)index {
-    NSLog(@"select at index %zd", index);
+    if (index < 0 || index >= [self.vctrlArray count]) {
+        return;
+    }
+    
+    self.selectedIndex = index;
+    // 跳转到指定页面
+    NSArray *showController = @[self.vctrlArray[self.selectedIndex]];
+    [self.pageViewController setViewControllers:showController direction:UIPageViewControllerNavigationDirectionForward animated:NO completion:nil];
 }
+
+#pragma mark - UIPageViewController DataSource
+
+-(UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController {
+    NSUInteger index = [self indexOfViewController:viewController];
+    if (index == NSNotFound) {
+        return nil;
+    }
+    
+    index --;
+    return [self viewControllerAtIndex:index];
+}
+
+-(UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(UIViewController *)viewController {
+    NSUInteger index = [self indexOfViewController:viewController];
+    if (index == NSNotFound ) {
+        return nil;
+    }
+    
+    index ++;
+    return [self viewControllerAtIndex:index];
+}
+
+#pragma mark - UIPageViewController Delegate
+
+-(void)pageViewController:(UIPageViewController *)pageViewController willTransitionToViewControllers:(NSArray *)pendingViewControllers {
+    NSUInteger index = [self indexOfViewController:pendingViewControllers.firstObject];
+    self.selectedIndex = index;
+}
+
+-(void)pageViewController:(UIPageViewController *)pageViewController didFinishAnimating:(BOOL)finished previousViewControllers:(NSArray *)previousViewControllers transitionCompleted:(BOOL)completed {
+    if (completed) {
+        [self.tabBar selectedAtIndex:self.selectedIndex];
+    }
+}
+
 
 @end
